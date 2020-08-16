9a45fe54c5726d98b21ce66f0f1c254545e99a01
Amulya <amulyausem@gmail.com>
2020-08-16 16:28:18 +0530

    Code of auto-generated email

---
+++ email.php
@@ -0,0 +1,45 @@
+<?php  
+include('preincludes/dbh.php');
+include('preincludes/session.php');
+
+$to=$_SESSION["classid"];
+                          
+
+require_once('C:\Users\varsh\Desktop\new\htdocs\emss\class.phpmailer.php');
+
+$mail = new PHPMailer;
+$email='usemamulya@gmail.com';
+
+  if(!$mail->validateAddress($email)){
+    echo 'Invalid Email Address';
+    exit;
+  }
+
+
+  $email_body = "";
+  $email_body .= "Assignment successfully submitted";
+
+  $mail->IsSMTP();
+  $mail->SMTPAuth = true;
+  $mail->SMTPSecure = "tls"; 
+  $mail->Host = "smtp.gmail.com";
+  $mail->Port = 587;
+  $mail->Username = "usemamulya@gmail.com";
+  $mail->Password = "Amulya,123";
+  
+  $mail->setFrom($email, 'amulya');
+  $mail->addAddress($to, 'Student'); 
+  $mail->isHTML(true);                               
+  $mail->Subject = "Upadate on assignment submission";
+  $mail->Body = $email_body;
+
+  if(!$mail->send()) {
+    echo 'Message could not be sent. ';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    exit;
+  }
+  //header
+mysqli_close($link);
+?>
+
+
