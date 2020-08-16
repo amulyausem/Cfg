98cd2baf394f9879ddda8a37c6a3b3d5bbcf18f7
Suhas R N <uzumakinaruto1582@gmail.com>
2020-08-16 15:00:33 +0530

    Adding JS

---
+++ Views/Teachers/index.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("add_button");
+button.addEventListener("click", () => {
+  var popup = document.getElementById("popup");
+  popup.classList.toggle("none");
+  console.log(classList);
+});
